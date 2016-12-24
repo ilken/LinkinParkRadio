ea0f627a1e0bbaa6552abdbc496b91f608ac400e
ILKEN BAHCECIOGLU <ILKEN BAHCECIOGLU>
2016-12-24 16:31:43 +0000

    Footer

---
+++ src/js/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+	render () {
+		return (
+			<footer className="footer container">
+				<div className="row">
+					<div className="col-md-12 col-sm-12 col-xs-12">
+						<p className="text-muted">Ilken Bahcecioglu - 25/12/2016</p>
+					</div>
+				</div>
+			</footer>
+		);
+	}
+}
